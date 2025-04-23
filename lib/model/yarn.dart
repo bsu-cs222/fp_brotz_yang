22e5f3f8e29281c5c4d9c2e9094c031dbbbf1c6c
emma <emma.brotz@gmail.com>
2025-04-23 17:41:44 -0400

    Refactor Yarn class to store swatchColor as a Color object

--- lib/model/yarn.dart
+++ lib/model/yarn.dart
@@ -1,8 +1,10 @@
-// represents a single skein in the user's inventory
+// Represents a single skein in the user's inventory
+import 'package:flutter/material.dart';
 
 class Yarn {
   final String _name;
   final String _color;
+  final Color _swatchColor;
   final String _fiber;
   final int _quantity;
   final String _brand;
@@ -10,17 +12,20 @@ class Yarn {
   Yarn({
     required String name,
     required String color,
+    required Color swatchColor,
     required String fiber,
     required int quantity,
     required String brand,
   }) : _name = name,
        _color = color,
+       _swatchColor = swatchColor,
        _fiber = fiber,
        _quantity = quantity,
        _brand = brand;
 
   String get name => _name;
   String get color => _color;
+  Color get swatchColor => _swatchColor;
   String get fiber => _fiber;
   int get quantity => _quantity;
   String get brand => _brand;
